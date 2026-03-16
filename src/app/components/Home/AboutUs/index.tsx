5e9dfa05f09eb1d9e931de417716ea7cd9c5bb4b
Johannes Karasik <johanneskarasik@Johanness-MacBook-Pro.local>
2026-03-16 02:11:20 +0100

    Update project

--- src/app/components/Home/AboutUs/index.tsx
+++ src/app/components/Home/AboutUs/index.tsx
@@ -159,7 +159,7 @@ const ReviewCard = ({
   }
 }) => {
   return (
-    <div className='rounded-[28px] border border-black/5 bg-white p-7 shadow-[0_8px_24px_rgba(0,0,0,0.06)]'>
+    <div className='inline-block w-full rounded-[28px] border border-black/5 bg-white p-7 align-top shadow-[0_8px_24px_rgba(0,0,0,0.06)]'>
       <div className='mb-6 flex items-start justify-between gap-4'>
         <StarRow />
         <span className='whitespace-nowrap text-[15px] font-medium text-[#6b778c]'>
@@ -259,7 +259,7 @@ const Testimonials = () => {
             {extendedReviews.map((review, index) => (
               <div
                 key={`${review.name}-${review.date}-${index}`}
-                className='shrink-0 px-3 md:w-1/2 xl:w-1/4'
+                className='shrink-0 self-start px-3 md:w-1/2 xl:w-1/4'
               >
                 <ReviewCard review={review} />
               </div>

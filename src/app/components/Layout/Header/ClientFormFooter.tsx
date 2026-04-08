180f35d01ffcf18c8897942e275c28d998b84c27
JohannesKarasik <johanneskarasik@Johanness-MacBook-Pro.local>
2026-04-08 17:45:39 +0200

    Initial commit

--- src/app/components/Layout/Header/ClientFormFooter.tsx
+++ src/app/components/Layout/Header/ClientFormFooter.tsx
@@ -7,19 +7,7 @@ import Script from 'next/script'
 const ClientFormFooter = () => {
   return (
     <>
-      <footer className='bg-[#07111f] py-10' id='Footer'>
-        <div className='mx-auto flex max-w-7xl items-center justify-center px-4 sm:px-6 lg:px-8'>
-          <Link href='/' className='flex items-center'>
-            <Image
-              src='/images/logodark.png'
-              alt='Logo'
-              width={160}
-              height={48}
-              className='h-auto w-auto max-h-12'
-            />
-          </Link>
-        </div>
-      </footer>
+
 
       <Script
         src='https://beta.leadconnectorhq.com/loader.js'
